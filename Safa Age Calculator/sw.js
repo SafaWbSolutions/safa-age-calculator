4d7e3ac30a3a57dfdbd838383ba130cbe52dfd9b
Safa Web Solutions <starservices0026@gmail.com>
2026-06-02 03:21:25 +0530

    Add files via upload

--- Safa Age Calculator/sw.js
+++ Safa Age Calculator/sw.js
@@ -9,12 +9,15 @@ const ASSETS_TO_CACHE = [
   '/icon-512.png'
 ];
 
-// Install Event - Cache assets
+// Install Event - Cache assets safely
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => {
-        return cache.addAll(ASSETS_TO_CACHE);
+        // Cache files one by one so if one fails, others still get cached
+        return Promise.allSettled(
+          ASSETS_TO_CACHE.map(url => cache.add(url).catch(err => console.log('SW Cache Error:', url, err)))
+        );
       })
       .then(() => self.skipWaiting())
   );
